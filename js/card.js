eef53892e0a2b1ac11c4a34b0cc03b1b2f89cd7e
Jason B. Cox <jcox1@linkedin.com>
2016-05-15 17:41:08 -0700

    Added RcAspectContainer and RcCard

---
+++ js/card.js
@@ -0,0 +1,32 @@
+
+window.RcAspectContainer = React.createClass({
+  render: function() {
+    var containerClasses =        'rc-aspect-container ' +          this.props.className;
+    var dummyClasses =            'rc-aspect-container-dummy-' +    this.props.height;
+    var contentContainerClasses = 'rc-aspect-container-content ' +  this.props.containerClasses;
+    return (
+      <div className={containerClasses}>
+        <div className={dummyClasses}></div>
+        <div className={contentContainerClasses}>
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
+});
+
+window.RcCard = React.createClass({
+  render: function() {
+    var containerClasses = 'rc-card-container ' + this.props.className;
+    var height = this.props.height || "100";
+    var innerClasses = this.props.innerClasses || "padding-normal";
+    return (
+      <div className={containerClasses}>
+        <RcAspectContainer className="rc-card" height={height} containerClasses={innerClasses}>
+          {this.props.children}
+        </RcAspectContainer>
+      </div>
+    )
+  }
+});
+
--- render_index.js
+++ render_index.js
@@ -35,9 +35,23 @@ var TestButton = React.createClass({
   }
 });
 
+var TestCards = React.createClass({
+  render: function() {
+    return (
+      <div className="row">
+        <RcCard className="col s12 m6 l3">These cards maintain their aspect ratio.</RcCard>
+        <RcCard className="col s12 m6 l3" height="50">You can modify the ratio with the 'height' property (in increments of 5 from 0 - 100 inclusive).</RcCard>
+        <RcCard className="col s12 m6 l3" innerClasses="padding-huge">Modifying 'innerClasses' property allows you to change the CSS of the inner container.</RcCard>
+        <RcCard className="col s12 m6 l3">Try viewing this page on different screen sizes to see how well these cards work with the grid!</RcCard>
+      </div>
+    )
+  }
+});
+
 ReactDOM.render(
   <div>
     <TestButtons />
+    <TestCards />
     <RcFooter />
   </div>,
   document.getElementById('container')
